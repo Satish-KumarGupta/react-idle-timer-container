5d0cb9411e454b8c86601a2ffb141ebbe0cffbe4
satish122 <satishkumar998475@gmail.com>
2021-12-01 17:47:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-2jeymh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-2jeymh)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import IdleTimerContainer from './Component/IdleTimerContainer';
+
+function App () {
+  return (
+    <div className='App'>
+      <IdleTimerContainer />
+    </div>
+  )
+}
+
+export default App
